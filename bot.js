1a7a288f577fca0cbbd839a7fffff525cfe5d37e
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2018-11-26 00:17:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,6 +20,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'سلام عليكم') {
+    	message.reply('وعليكم السلام'
+});
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
