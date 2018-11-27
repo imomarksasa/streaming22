58743feebafa1dd475cb147f72cc193cf1136c73
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2018-11-27 22:34:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,29 @@ client.on('ready', () => {
 
 
 
-client.on('ready', () => {
-    client.user.setGame(`ياخايننء الءك يوم تجي فيه وتندم~x`,"http://twitch.tv/y04zgamer")
-    client.user.setStatus("dnd")
-  console.log(`Logged in as ${client.user.tag}!`);
+const Discord = require("dicord.js");
+const client = new Discord.Client();
+
+
+
+client.on("ready", () => {
+client.user.setPresence({
+  status: 'dnd',
+  game: { 
+     type: 0,
+     name: 'Surprise Mother Fucker :smiley: ',
+     details: I'm : Admin - Developer and Programmer,
+     url: 'http://twitch.tv/Streammingg',
+     state: إنْ لَمْ تَجِد لكْ حآقدْ إعلمْ أنْك إنسآن فآشِلْ,
+    application_id: '281376075802476544',
+     assets: {
+        small_image: 366835431037337600,
+        small_text: ' Take This ! ' ,
+        large_image: 370451271133429760,
+        large_text: ҜṦẰ FOREVER ♥ }
+
+  }
+    });
 });
 
 
