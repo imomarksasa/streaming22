53814282c81d358db655c4bbabefa6b6e7cf46c6
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2019-01-26 20:14:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,56 +1,64 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+ const prefix = "+";
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message' , message => {
-        if(message.content === `<@${client.user.id}>`) {
-            message.reply("امر")
-        }
-    });
-
-
-
-client.on('ready', () => {
-    client.user.setGame(`Bo,HsTRai`,"https://twitch.tv/sliman0ali")
-    client.user.setStatus("dnd")
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-
-
-
-
-
-
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+    if (message.content === 'zg') {
+        message.reply('pong');
+      }
 });
 
+client.on('ready',  () => {
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+  console.log('by BadGuY');
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log(`Logged in as  * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log('is online')
+client.user.setStatus("dnd");
+});
+
+
+
+
+
+
+const devs = ["384697938770657281"];
+const adminprefix = ["-"];
 client.on('message', message => {
-    if (message.content === 'سلام عليكم') {
-    	message.reply('وعليكم السلام');
-  	}
-});
-
-client.on('guildMemberAdd', member => {
-    let channel = member.guild.channels.find('name', 'public-chat');
-    let memberavatar = member.user.avatarURL
-      if (!channel) return;
-    let embed = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setThumbnail(memberavatar)
-        .addField(' نورت السيرفر يا قلبي' , `Welcome to the server, ${member}`)
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
       
-   
-      channel.sendEmbed(embed);
-    });
+  if (message.content.startsWith('ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+     if (message.content === ("leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith('wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith('mils')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith('st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**✅**`)
+  }
+  if (message.content.startsWith('setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith('setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
 
-
-
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
